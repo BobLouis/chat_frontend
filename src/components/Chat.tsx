e74307f9113c4103fea1077c467ece97d630b891
BobLouis <bob900316@gmail.com>
2024-02-16 03:23:45 +0800

    up

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -4,6 +4,8 @@ import { AuthContext } from "../contexts/AuthContext";
 import { useParams } from "react-router-dom";
 import { MessageModel } from "../models/Message";
 import { Message } from "./Message";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { ChatLoader } from "./ChatLoader";
 export function Chat() {
     const { conversationName } = useParams();
     const [welcomeMessage, setWelcomeMessage] = useState("");
@@ -11,6 +13,36 @@ export function Chat() {
 
     const [messageHistory, setMessageHistory] = useState<any>([]);
     const { user } = useContext(AuthContext);
+
+    const [page, setPage] = useState(2);
+    const [hasMoreMessages, setHasMoreMessages] = useState(false);
+
+    async function fetchMessages() {
+        const apiRes = await fetch(
+            `http://127.0.0.1:8000/chats/?conversation=${conversationName}&page=${page}`,
+            {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Authorization: `token ${user?.token}`
+                }
+            }
+        );
+        if (apiRes.status === 200) {
+            const data: {
+                count: number;
+                next: string | null; // URL
+                previous: string | null; // URL
+                results: MessageModel[];
+            } = await apiRes.json();
+            setHasMoreMessages(data.next !== null);
+            setPage(page + 1);
+            setMessageHistory((prev: MessageModel[]) => prev.concat(data.results));
+        }
+    }
+
+
     const webSocketUrl = user ? `ws://127.0.0.1:8000/chat/${conversationName}/?token=${user.token}` : null;
     const { readyState } = useWebSocket(webSocketUrl, {
         // queryParams: {
@@ -29,10 +61,12 @@ export function Chat() {
                     setWelcomeMessage(data.message);
                     break;
                 case 'chat_message_echo':
-                    setMessageHistory((prev: any) => prev.concat(data.message));
+                    // setMessageHistory((prev: any) => prev.concat(data.message));
+                    setMessageHistory((prev: any) => [data.message, ...prev]);
                     break;
                 case "last_50_messages":
                     setMessageHistory(data.messages);
+                    setHasMoreMessages(data.has_more);
                     break;
                 default:
                     console.error("Unknown message type!");
@@ -98,11 +132,33 @@ export function Chat() {
             </button>
             <hr />
 
-            <ul className="mt-3 flex flex-col-reverse relative w-full border border-gray-200 overflow-y-auto p-6">
+            {/* <ul className="mt-3 flex flex-col-reverse relative w-full border border-gray-200 overflow-y-auto p-6">
                 {messageHistory.map((message: MessageModel) => (
                     <Message key={message.id} message={message} />
                 ))}
-            </ul>
+            </ul> */}
+
+            <div
+                id="scrollableDiv"
+                className="h-[20rem] mt-3 flex flex-col-reverse relative w-full border border-gray-200 overflow-y-auto p-6"
+            >
+                <div>
+                    {/* Put the scroll bar always on the bottom */}
+                    <InfiniteScroll
+                        dataLength={messageHistory.length}
+                        next={fetchMessages}
+                        className="flex flex-col-reverse" // To put endMessage and loader to the top
+                        inverse={true}
+                        hasMore={hasMoreMessages}
+                        loader={<ChatLoader />}
+                        scrollableTarget="scrollableDiv"
+                    >
+                        {messageHistory.map((message: MessageModel) => (
+                            <Message key={message.id} message={message} />
+                        ))}
+                    </InfiniteScroll>
+                </div>
+            </div>
 
         </div>
     );
